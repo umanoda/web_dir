e680ec29771fc7d1b8d440ec6dfe285233a68b0a
hoshino <hhenpitu@gmail.com>
2017-02-25 14:20:53 +0900

    directory has children directories

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -1,3 +1,5 @@
 class Directory < ApplicationRecord
   has_many :items, dependent: :destroy
+  has_many :child, class_name: self, foreign_key: :parent_id, dependent: :destroy
+  belongs_to :parent, class_name: self, optional: true
 end
